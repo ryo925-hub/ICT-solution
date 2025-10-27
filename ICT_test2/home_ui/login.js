69591c0544a4076f460129922c909def1d3adac4
ryo925-hub <ryocona925@gmail.com>
2025-10-27 14:57:56 +0900

    Add files via upload

---
+++ ICT_test2/home_ui/login.js
@@ -0,0 +1,30 @@
+import createHttpClient from '../../utils/HttpClient.js';
+
+const { sendRequest } = createHttpClient('dev');
+
+// const res = await sendRequest('Login/Regist', {
+//     firstName1: "リョウ",
+//     lastName1: "スギヤマ",
+//     firstName2: "涼",
+//     lastName2: "杉山",
+//     birthday: new Date("2002-09-25"),
+//     mailAddress: "abcd@gmail.com",
+//     password: "password",
+// })
+
+
+async function login() {
+    const loginId = document.getElementById('loginId').value;
+    const password = document.getElementById('password').value;
+
+    const res = await sendRequest("Login/Login", {
+        loginID: loginId,
+        password: password,
+    })
+
+    if (res.isSucc) {
+        console.log(res.res.token);
+        localStorage.setItem('token', res.res.token);
+        window.location.href = 'index.html';
+    }
+}
